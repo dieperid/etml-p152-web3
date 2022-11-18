62f1397e83c30f6c03eb21a3331784b59328cfbf
dieperid <david.dieperink@ik.me>
2022-11-18 16:03:30 +0100

    Twig template

--- luxsure-site/src/Controller/UserController.php
+++ luxsure-site/src/Controller/UserController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class UserController
+class UserController extends AbstractController
 {
     #[Route('/')]
     public function homepage() : Response
     {
-       return new Response('Title : Luxsure');
+        $shoes = [
+            ['shoe' => 'Air Force 1', 'artist' => 'David'],
+            ['shoe' => 'Jordan 4', 'artist' => 'David'],
+            ['shoe' => 'New Balance 2002R', 'artist' => 'David'],
+            ['shoe' => 'Air Max Plus TN', 'artist' => 'David'],
+        ];
+
+       return $this->render('user/user.html.twig', [
+           'title' => 'Luxsure Shoes',
+           'shoes' => $shoes,
+       ]);
     }
 
     #[Route('/browse/{slug}')]
