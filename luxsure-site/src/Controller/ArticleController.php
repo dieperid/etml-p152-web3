85c1d502f21d3bf891c396ac82705702a73bd7de
dieperid <david.dieperink@ik.me>
2022-12-02 08:54:03 +0100

    HomeController + redirection on index.htmÃl.twig

---
+++ luxsure-site/src/Controller/ArticleController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    #[Route('/article', name: 'create_article')]
+    public function createArticle(ManagerRegistry $doctrine, string $name, string $description, float $price, int $quantity, string $image): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $article = new Article();
+        $article->setName($name);
+        $article->setDescription($description);
+        $article->setPrice($price);
+        $article->setQuantity($quantity);
+        $article->setImage($image);
+
+        // tell Doctrine to save the article
+        $entityManager->persist($article);
+        // execute the query
+        $entityManager->flush();
+
+        return new Response('Savec new article with id :'.$article->getId());
+    }
+
+    #[Route('/article/edit/{id}', name: 'article_edit')]
+    public function updateArticle(ManagerRegistry $doctrine, int $id)
+    {
+        $entityManager = $doctrine->getManager();
+        $article = $entityManager->getRepository(Article::class)->find($id);
+
+        if(!$article){
+            throw $this->createNotFoundException(
+                'No article found for id $id'
+            );
+        }
+    }
+}
